32f430498aa37ba478c3ac48ec7defcf28ff6421
Omkar Jethe <98147636+omkarsjethe@users.noreply.github.com>
2025-11-29 11:46:16 +0530

    Add files via upload

---
+++ add.java
@@ -0,0 +1,13 @@
+public class add {
+    public static void main(String [] args) {
+
+        int a = 25 ;
+        int b = 35;
+        int c = a + b ;
+ 
+        System.out.println("Value of a is :"+ a);
+        System.out.println("Value of b is :"+ b);
+        System.err.println("Value of c is :"+ c);
+        
+    }   
+}
---
+++ demo.java
@@ -0,0 +1,6 @@
+public class demo{
+  public static void main(String[] args) {
+    String name = "Omkar";
+    System.out.println("Hello " + name);
+  }
+}
