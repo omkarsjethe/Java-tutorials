6dfb4f7107332b1e8f3822b2c27c911778b28126
Omkar Jethe <98147636+omkarsjethe@users.noreply.github.com>
2025-12-27 11:59:22 +0530

    Add files via upload

---
+++ welcome.java
@@ -0,0 +1,9 @@
+public class welcome {
+  public static void welcome(String name){
+    System.out.println("Welcome to Java Tutorials, " + name + "!");
+  }
+    public static void main(String[] args) {
+      welcome("Omkar");
+      welcome("Yash");
+    }
+}
