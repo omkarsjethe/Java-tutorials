1c7a58097483a02166870a3a4916c7f93ebefa34
Omkar Jethe <98147636+omkarsjethe@users.noreply.github.com>
2025-12-27 11:59:57 +0530

    Add files via upload

---
+++ calculate.java
@@ -0,0 +1,10 @@
+public class calculate {
+    public static int multiply(int a,int b){
+        return a*b ;
+    }
+    public static void main(String[] args){
+        int result = multiply (5,6);
+        System.out.println("The multiplication is: " + result);
+    }
+    
+}
