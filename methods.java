d903c97faebadcacb78bffaf5430a1974f82edbd
Omkar Jethe <98147636+omkarsjethe@users.noreply.github.com>
2025-12-27 11:58:54 +0530

    Add files via upload

---
+++ methods.java
@@ -0,0 +1,8 @@
+public class methods {
+    public static void methods(String date ){
+        System.out.println("This is welcome message from methods"+date+" is todays date");
+    }
+    public static void main(String[] args) {
+        methods("6th June 2024");
+    }
+}
